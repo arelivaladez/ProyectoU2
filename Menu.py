4f59207aafe7d42cb162e5886606779bc40c5662
arelivaladez <arelivaladez0@gmail.com>
2022-03-16 12:54:42 -0600

    Primera parte

---
+++ Menu.py
@@ -0,0 +1,19 @@
+#Muenu
+print("Menu;")
+Num= print("1 = Numeros fibonacci;")
+area= print ("2= Area del circulo;")
+r= print (input ("¿Que quieres realizar?;"))
+
+Segun r Hacer
+		1:
+			print ("Area Circulo")
+            pi = 3.14
+
+            radio = int(input("Ingresa el valor del radio: "))
+            area = (radio ** 2) * pi
+
+            print ("El area es ",  area)
+		2:
+			
+
+
